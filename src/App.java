a82c9d9248853971e46c84022e6f550f4b040803
yashindijayawardhana <yashindijayawardhana@gmail.com>
2025-02-11 23:13:56 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+}
---
+++ src/Wrapper.java
@@ -0,0 +1,5 @@
+public class Wrapper {
+    public static void main(String[] args) throws Exception {
+        App.main(args);
+    }
+}
